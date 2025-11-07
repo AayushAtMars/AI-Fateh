77201ce1d6f12733814c9022ad3f078321fd94ed
AayushAtMars <yoaayush14@gmail.com>
2025-11-07 11:07:48 +0530

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import TargetCursor from './components/cursor/TargetCursor'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <TargetCursor spinDuration={2} hideDefaultCursor={true}/>
       <Navbar/>
       <Hero/>
       
